1ed4c3887fd9195706ce5055321ec415a7766668
Juan-GR123 <juangarros2005@gmail.com>
2025-02-21 09:56:30 +0100

    login

--- Proyecto_fin_curso_2/controllers/UsuarioController.php
+++ Proyecto_fin_curso_2/controllers/UsuarioController.php
@@ -25,7 +25,7 @@ class usuarioController
                 $usuario->setApellidos($apellidos);
                 $usuario->setEmail($email);
                 $usuario->setPassword($password);
-                
+
                 $save = $usuario->save();
                 if($save){
                     $_SESSION['register'] = "complete";
@@ -40,4 +40,43 @@ class usuarioController
         }    
         header("Location:" .base_url.'usuario/registro');
     }
-}
+
+    public  function login(){
+        if(isset($_POST)){
+            // Identificar al usuario
+            // Consulta la base de datos
+            $usuario = new Usuario();
+            $usuario->setEmail($_POST['email']);
+            $usuario->setPassword($_POST['password']);
+
+            $identity = $usuario->login(); //devuelve un objeto declarado en usuario
+
+            if($identity && is_object($identity)){
+                $_SESSION['identity'] = $identity;
+
+                if($identity->role == 'admin'){
+                    $_SESSION['admin'] = true;
+                }
+            }else{
+                $_SESSION['error_login'] = 'Identificación fallida';
+            }
+
+        }
+        header("Location:" . base_url);
+    }
+
+    public function logout(){
+
+        if(isset($_SESSION['identity'])){
+            unset($_SESSION['identity']);
+        }
+
+        if(isset($_SESSION['admin'])){
+            unset($_SESSION['admin']);
+        }
+
+        header("Location: " .base_url);
+    }
+
+
+}//Fin de la clase
--- Proyecto_fin_curso_2/views/layout/sidebar.php
+++ Proyecto_fin_curso_2/views/layout/sidebar.php
@@ -2,18 +2,23 @@
 <aside id="lateral">
     <div id="login" class="block">
         <h3>Entrar a la Web</h3>
-        <form action="#" method="post">
+        <?php if(!isset($_SESSION['identity'])): ?>
+        <form action="<?=base_url?>usuario/login" method="post">
             <label for="email">Email</label>
             <input type="email" name="email">
             <label for="password">Password</label>
             <input type="password" name="password">
             <input type="submit" value="Enviar">
         </form>
+        <?php else: ?>
+            <h3><?=$_SESSION['identity']->nombre?> <?=$_SESSION['identity']->apellidos?></h3>
+        <?php endif; ?>
 
         <ul>
             <li><a href="#">Mis pedidos</a></li>
             <li><a href="#">Gestionar pedidos</a></li>
             <li><a href="#">Gestionar categorias</a></li>
+            <li><a href="<?=base_url?>usuario/logout">Cerrar Sesión</a></li>
         </ul>
     </div>
 
