285c12485967b5f0569209a3ede41e2958da16be
Juan-GR123 <juangarros2005@gmail.com>
2025-03-02 13:54:54 +0100

    base_datos

---
+++ Proyecto_fin_curso_2/database/tienda.sql
@@ -0,0 +1,253 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-03-2025 a las 13:51:55
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`) VALUES
+(1, 'tapa dura'),
+(2, 'tapa blanda'),
+(3, 'Edición Limitada'),
+(4, 'Edición de bolsillo'),
+(7, 'Novedades');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lineas_pedidos`
+--
+
+CREATE TABLE `lineas_pedidos` (
+  `id` int(255) NOT NULL,
+  `pedido_id` int(255) NOT NULL,
+  `producto_id` int(255) NOT NULL,
+  `unidades` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `lineas_pedidos`
+--
+
+INSERT INTO `lineas_pedidos` (`id`, `pedido_id`, `producto_id`, `unidades`) VALUES
+(14, 35, 2, 1),
+(15, 36, 2, 1),
+(16, 36, 5, 1),
+(18, 38, 1, 1),
+(37, 58, 1, 1),
+(38, 59, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id` int(255) NOT NULL,
+  `usuario_id` int(255) NOT NULL,
+  `provincia` varchar(100) NOT NULL,
+  `localidad` varchar(100) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `coste` float(200,2) NOT NULL,
+  `estado` varchar(100) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id`, `usuario_id`, `provincia`, `localidad`, `direccion`, `coste`, `estado`, `fecha`, `hora`) VALUES
+(35, 12, 'Alicante', 'Alicante', 'Calle Alcala nº1 piso 2, Puerta c', 23.00, 'confirm', '2025-02-28', '22:17:21'),
+(36, 12, 'Madrid', 'Madrid', 'fdasasd', 63.00, 'confirm', '2025-02-28', '22:17:46'),
+(38, 12, 'pep', 'pep', 'fdasasd', 20.00, 'preparation', '2025-02-28', '23:08:33'),
+(58, 12, 'Madrid', 'Madrid', 'Calle Alcala nº1 piso 2, Puerta c', 20.00, 'ready', '2025-03-01', '17:00:16'),
+(59, 31, 'Madrid', 'Madrid', 'Calle Alcala nº1 piso 2, Puerta c', 46.00, 'confirm', '2025-03-02', '12:49:14');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(255) NOT NULL,
+  `categoria_id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `precio` float(100,2) NOT NULL,
+  `stock` int(255) NOT NULL,
+  `oferta` varchar(2) NOT NULL,
+  `fecha` date NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `categoria_id`, `nombre`, `descripcion`, `precio`, `stock`, `oferta`, `fecha`, `imagen`) VALUES
+(1, 7, 'Alas de fuego', 'Libro de fantasia', 20.00, 4, '0', '2025-02-25', '91YXlIlJTPL._AC_UF1000,1000_QL80_.jpg'),
+(2, 7, 'Alas de sangre', 'libro de fantasia', 23.00, 34, '0', '2025-02-25', '91UJR77PRFL._UF1000,1000_QL80_.jpg'),
+(5, 7, 'Libro nuevo', 'Este es el libro nuevo 2', 40.00, 120, '0', '2025-02-25', 'portada-libros-100px.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` varchar(20) NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `rol`, `imagen`) VALUES
+(1, 'Admin', 'Admin', 'admin@admin.com', 'contraseña', 'admin', NULL),
+(3, 'Pepe', 'algo', 'algo@algo.com', '$2y$04$WRDHS0ncRWwxgDeagwEfPO4Bpuq4LjwnIwYxfGeRDGt1eWKYBM.q.', 'user', NULL),
+(12, 'PE', 'Calavera', 'algo3@algo.com', '$2y$04$JLSVg3uhZjTGeZuCV3JZzOVHiAMufcpkLq4l3MPjv19VZTQkh.j.i', 'admin', NULL),
+(23, 'sas', 'algo', 'algo4@algo.com', '$2y$04$suVvkP0/TSQLnppZSo4mCuQY9MyoWhNyMM2w5tQYe9IcTnaX2xCCm', 'admin', NULL),
+(31, 'prueba2', 'algo2', 'algo5@algo.com', '$2y$04$jEVtgJXOknJVuFnfW6PAFObcYjNm.xt5R0e.n/ETJ0xqaiMj9mAUS', 'admin', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `lineas_pedidos`
+--
+ALTER TABLE `lineas_pedidos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_linea_pedido` (`pedido_id`),
+  ADD KEY `fk_linea_producto` (`producto_id`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pedido_usuario` (`usuario_id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_producto_categoria` (`categoria_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `lineas_pedidos`
+--
+ALTER TABLE `lineas_pedidos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `lineas_pedidos`
+--
+ALTER TABLE `lineas_pedidos`
+  ADD CONSTRAINT `fk_linea_pedido` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`id`),
+  ADD CONSTRAINT `fk_linea_producto` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`);
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `fk_pedido_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
